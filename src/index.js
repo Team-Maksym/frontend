3b62f68ee378b8389902cca0d8ae2c5f95d00fe5
Dan <dan.shirayy@gmail.com>
2023-03-22 17:03:40 +0200

    0.3 task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { ThemeProvider, createTheme } from '@mui/material';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+    secondary: {
+      main: '#f39200',
+    },
+    neutral: {
+      white: '#ffffff',
+      lightGrey: '#999999',
+      grey: '#6B6C6F',
+    },
+  },
+  typography: {
+    fontFamily: ['Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'].join(','),
+    h1: {
+      fontSize: 64,
+      fontWeight: 800,
+      lineHeight: '96px',
+    },
+    h2: {
+      fontSize: 48,
+      fontWeight: 700,
+      lineHeight: '58px',
+    },
+    h3: {
+      fontSize: 36,
+      fontWeight: 700,
+      lineHeight: '44px',
+    },
+    h4: {
+      fontSize: 28,
+      fontWeight: 700,
+      lineHeight: '28px',
+    },
+    h5: {
+      fontSize: 24,
+      fontWeight: 700,
+      lineHeight: '24px',
+    },
+    h6: {
+      fontSize: 20,
+      fontWeight: 700,
+      lineHeight: '24px',
+    },
+    subtitle1: {
+      fontSize: 14,
+      fontWeight: 700,
+      lineHeight: '18px',
+    },
+  },
+});
+root.render(
+  <ThemeProvider theme={theme}>
+    <App></App>
+  </ThemeProvider>,
+);
