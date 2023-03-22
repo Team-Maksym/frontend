227dcde6cc6d68b8bb951859350599b03b4e9cd1
Dan <dan.shirayy@gmail.com>
2023-03-22 20:05:06 +0200

    0.3 Task 0.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,6 @@
 import React from 'react';
-import { Button, Typography } from '@mui/material';
-function App() {
-  return (
-    <div className="App">
-      <Button sx={{ background: 'primary.main' }} variant="contained">
-        Button 2
-      </Button>
-      <Typography sx={{ color: 'primary.main' }} variant="h1">
-        Aboba
-      </Typography>
-      <Typography sx={{ color: 'secondary.main' }} variant="h2">
-        Aboba
-      </Typography>
-      <Typography sx={{ color: 'neutral.white' }} variant="h3">
-        Aboba
-      </Typography>
-      <Typography sx={{ color: 'neutral.lightGrey' }} variant="h4">
-        Aboba
-      </Typography>
-      <Typography sx={{ color: 'neutral.grey' }} variant="h5">
-        Aboba
-      </Typography>
-      <Typography sx={{ color: 'primary.main' }} variant="h6">
-        Aboba
-      </Typography>
-      <Typography sx={{ color: 'primary.main' }} variant="subtitle1">
-        Aboba
-      </Typography>
-    </div>
-  );
-}
-
-export default App;
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme/theme';
+export const App = () => {
+  return <ThemeProvider theme={theme}></ThemeProvider>;
+};
