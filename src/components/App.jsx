cbb92ce2ca612ddd3485803e89a55603d50e205a
Anna <annasukharieva0@gmail.com>
2023-03-22 20:54:36 +0200

    1.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
+import { SmallTalentCard } from "./smallTalentCard";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
