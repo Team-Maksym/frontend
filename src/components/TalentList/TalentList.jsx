c23a03eeef9f12c49caf8dc4684e1872f008c3e3
Anna <annasukharieva0@gmail.com>
2023-03-28 14:18:09 +0300

    1.1-v.7

--- src/components/TalentList/TalentList.jsx
+++ src/components/TalentList/TalentList.jsx
@@ -7,17 +7,24 @@ import { PaginationCustom } from './components/PaginationCustom';
 export const TalentList = () => {
   const [talents, setTalents] = useState([]);
 
+  const tals = [
+    {
+      full_name: 'Anna Sukharieva',
+      position: 'front-end'
+    }
+  ]
+
   const items = talents.map((item, index) => {
     return (
-      <Grid sx={{ minHeight: '255px', minWidth: '230px' }} item xs={1} key={index}>
+      <Grid sx={{ minHeight: '255px'}} item xs={1} key={index}>
         <SmallTalentCard talentName={item.full_name} position={item.position} avatar={item.avatar} />
       </Grid>
     );
   });
 
   return (
-    <Box sx={{ flexGrow: 1, m: '25px auto', p: '0 25px', width: '100%' }}>
-      <Grid container spacing={3} columns={5} row={2} sx={{ alignItems: 'stretch' }}>
+    <Box sx={{ flexGrow: 1, m: '25px auto', p: '0 25px'}}>
+      <Grid container spacing={3} columns={5} sx={{ alignItems: 'stretch' }}>
         {items}
       </Grid>
       <PaginationCustom setHook={setTalents} queryFunction={getAllTalents} />
--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,4 +1,4 @@
-import { Grid, Container } from '@mui/material';
+import { Container } from '@mui/material';
 
 export const Wrapper = (props) => {
   return (
