22d3fa63c8629b0a5974701ae70c74a9c8be9ad3
Anna <annasukharieva0@gmail.com>
2023-04-07 20:39:49 +0300

    log out refactoring 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,24 @@
-import { AppBar, Toolbar, Typography, Container, Button } from '@mui/material';
+import { AppBar, Toolbar, Container } from '@mui/material';
 import { LogoText } from './components/LogoText';
 import { Navbar } from './components/Navbar';
-import { useContext } from 'react';
+import { useState } from 'react';
+import { UnauthorizedNavbar } from './components/UnauthorizedNavbar';
 import { TalentContext } from '../../shared/context/TalentContext';
-import LoginIcon from '@mui/icons-material/Login';
-import { Tooltip } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 
 export const Header = () => {
+  const [autorized, setAutorized] = useState(!!localStorage.token);
   const { openAuthModal } = useContext(TalentContext);
-  const navigate = useNavigate();
 
   return (
     <AppBar sx={{ bgcolor: 'primary.main', color: 'neutral.white' }}>
       <Container>
         <Toolbar sx={{ flexWrap: 'wrap' }}>
           <LogoText />
-          <Navbar />
-          {!!localStorage.token ? (
-            <Tooltip title="Log out">
-              <Button
-                color="inherit"
-                size="large"
-                onClick={() => {
-                  localStorage.removeItem('token');
-                  navigate('/');
-                }}
-              >
-                <LoginIcon
-                  sx={{
-                    '&:hover': {
-                      color: 'secondary.main',
-                    },
-                  }}
-                />
-              </Button>
-            </Tooltip>
+          {autorized && !!localStorage.token ? (
+            <Navbar setAutorized={setAutorized} />
           ) : (
-            <>
-              <Button color="inherit" size="large">
-                <Typography variant="h6" noWrap onClick={() => openAuthModal('signIn')}>
-                  SIGN IN
-                </Typography>
-              </Button>
-              <Typography variant="h5" noWrap>
-                /
-              </Typography>
-              <Button color="inherit" size="large" onClick={() => openAuthModal('signUp')}>
-                <Typography variant="h6" noWrap>
-                  SIGN UP
-                </Typography>
-              </Button>
-            </>
+            <UnauthorizedNavbar setAutorized={setAutorized} openAuthModal={openAuthModal}></UnauthorizedNavbar>
           )}
         </Toolbar>
       </Container>
--- src/components/Header/components/Navbar/Navbar.jsx
+++ src/components/Header/components/Navbar/Navbar.jsx
@@ -1,18 +1,50 @@
 import { Tabs, Tab } from '@mui/material';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import LoginIcon from '@mui/icons-material/Login';
+import { Tooltip } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { useEffect } from 'react';
 
-export const Navbar = () => {
+export const Navbar = ({ setAutorized }) => {
   const [value, setValue] = useState('one');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentUrl = window.location.href;
+    if (currentUrl.includes('/profile')) {
+      setValue('two')
+    }
+  },[])
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const logOut = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+    setAutorized(false);
+  };
+
   return (
-    <Tabs value={value} onChange={handleChange} textColor="inherit" indicatorColor="secondary">
-      {!!localStorage.token && <Tab value="one" label="Profile" component={Link} to="/profile" />}
-    </Tabs>
+    <>
+      <Tabs value={value} onChange={handleChange} textColor="inherit" indicatorColor="secondary">
+        <Tab value="one" label="Home" component={Link} to="/" />
+        <Tab value="two" label="Profile" component={Link} to="/profile" />
+      </Tabs>
+      <Tooltip title="Log out">
+        <Button color="inherit" size="large"  onClick={() => logOut()}>
+          <LoginIcon
+            sx={{
+              '&:hover': {
+                color: 'secondary.main',
+              },
+            }}
+          />
+        </Button>
+      </Tooltip>
+    </>
   );
 };
-
