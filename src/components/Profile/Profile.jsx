ce6cfa26963a4093cf79264b997939db8cddb909
Dan <dan.shirayy@gmail.com>
2023-04-09 14:13:21 +0300

    0.7

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,14 @@
 import { Wrapper } from '../Wrapper';
-<<<<<<< HEAD
 import { BigTalentCard } from './components/BigTalentCard';
-import { Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { ProofMenu } from './components/ProofMenu';
-export const Profile = ({ logged }) => {
-  return (
-    <>
-      {logged && !!localStorage.token ? (
-        <Wrapper>
-          <Box sx={{ display: 'flex' }}>
-            <BigTalentCard talentName="Susan" position="front-end developer" />
-            <ProofMenu />
-          </Box>
-        </Wrapper>
-      ) : (
-        <Navigate to="/" />
-      )}
-    </>
-=======
-import { BigTalentCard } from './components/BigTalentCard/BigTalentCard';
-
 export const Profile = () => {
-
   return (
     <Wrapper>
-      <BigTalentCard />
+      <Box sx={{ display: 'flex' }}>
+        <BigTalentCard />
+        <ProofMenu />
+      </Box>
     </Wrapper>
->>>>>>> dev
   );
 };
