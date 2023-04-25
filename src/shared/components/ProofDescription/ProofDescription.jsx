92f13a30bf41aec736710eb79fcc8b7d5a020403
Dan <dan.shirayy@gmail.com>
2023-04-25 18:50:53 +0300

    improved fix

--- src/shared/components/ProofDescription/ProofDescription.jsx
+++ src/shared/components/ProofDescription/ProofDescription.jsx
@@ -1,8 +1,18 @@
 import { AccordionDetails, Box, Button, Link, Typography } from '@mui/material';
 export const ProofDescription = ({ description, link }) => {
+  const addSpace = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      if (i % 98 === 0 && i !== 0) {
+        result += ' ';
+      }
+      result += str[i];
+    }
+    return result;
+  };
   return (
     <AccordionDetails sx={{ bgcolor: 'primary.main', color: 'neutral.white', borderTop: 1, overflow: 'hidden' }}>
-      <Typography>{description}</Typography>
+      <Typography>{addSpace(description)}</Typography>
       <Box sx={{ display: 'flex', justifyContent: 'left', mt: '15px' }}>
         {link && (
           <Button variant="contained" sx={{ bgcolor: 'secondary.main' }}>
