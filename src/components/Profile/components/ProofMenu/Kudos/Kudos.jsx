5cda8cae597bf76dafb785d55b509d643b3ae916
Anna <annasukharieva0@gmail.com>
2023-05-08 19:36:53 +0300

    kudos messages

--- src/components/Profile/components/ProofMenu/Kudos/Kudos.jsx
+++ src/components/Profile/components/ProofMenu/Kudos/Kudos.jsx
@@ -5,28 +5,35 @@ import { getKudos, postKudos } from '../../../../../shared/service/KudosService/
 
 export const Kudos = ({ proofId, isKudosBtnShowing = true }) => {
   const [kudos, setKudos] = useState(null);
+  const [clickedKudos, setClickedKudos] = useState(false);
 
   useEffect(() => {
     getKudos(proofId).then((kudos) => {
       setKudos(() => kudos);
     });
-  }, [proofId]);
+    setClickedKudos(false);
+  }, [proofId, clickedKudos]);
 
   const onClickHandler = () => {
-    postKudos(proofId, 1).catch((error) => console.log(error));
-    // .then(
-    //   (kudos) => {
-    //     console.log("kudos", kudos);
-    //     // setKudos((prevState) => ({ kudos: { ...prevState.kudos, kudos_on_proof: prevState.kudos_on_proof + 1 } }));
-    //   },
-    //   (error) => {
-    //     if (error.response.status === 409) {
-    //       setIsKudasPlaced(() => true);
-    //     } else {
-    //       throw error;
-    //     }
-    //   },
-    // );
+    postKudos(proofId, 1)
+      .then(() => setClickedKudos(true))
+      .catch((error) => console.log(error));
+  };
+
+  console.log(kudos);
+
+  const message = (kudos) => {
+    if (kudos.kudos_on_proof && kudos.kudos_from_me) {
+      if (kudos.kudos_on_proof - kudos.kudos_from_me > 0) {
+        return `${kudos.kudos_from_me} your kudos and ${kudos.kudos_on_proof - kudos.kudos_from_me} others`;
+      } else {
+        return `${kudos.kudos_from_me} your kudos only`;
+      }
+    } else if (kudos.kudos_on_proof && !kudos.kudos_from_me) {
+      return `${kudos.kudos_on_proof} others kudos`;
+    } else {
+      return 'No one gave kudos to this proof yet';
+    }
   };
 
   return (
@@ -37,7 +44,7 @@ export const Kudos = ({ proofId, isKudosBtnShowing = true }) => {
           <Star sx={{ fontSize: 28, color: kudos?.is_kudosed ? 'secondary.main' : 'neutral.white' }} />
         </IconButton>
       }
-      label={kudos !== null ? kudos.kudos_on_proof : ''}
+      label={kudos && message(kudos)}
       sx={{
         bgcolor: 'neutral.whiteGrey',
         color: 'neutral.white',
