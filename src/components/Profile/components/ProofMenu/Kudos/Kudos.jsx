b32a0a4725f89f26de2281bfefda292eeead19b8
Anna <annasukharieva0@gmail.com>
2023-05-08 21:25:41 +0300

    interim result

--- src/components/Profile/components/ProofMenu/Kudos/Kudos.jsx
+++ src/components/Profile/components/ProofMenu/Kudos/Kudos.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import { Chip, IconButton } from '@mui/material';
 import { Star } from '@mui/icons-material';
 import { getKudos, postKudos } from '../../../../../shared/service/KudosService/KudosService';
+import { KudosGiveModal } from './components/KudosGiveModal';
 
 export const Kudos = ({ proofId, isKudosBtnShowing = true }) => {
   const [kudos, setKudos] = useState(null);
   const [clickedKudos, setClickedKudos] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     getKudos(proofId).then((kudos) => {
@@ -37,21 +39,27 @@ export const Kudos = ({ proofId, isKudosBtnShowing = true }) => {
   };
 
   return (
-    <Chip
-      disabled={!isKudosBtnShowing}
-      icon={
-        <IconButton aria-label={kudos !== null ? kudos.kudos_on_proof : ''} onClick={onClickHandler} sx={{ p: 0 }}>
-          <Star sx={{ fontSize: 28, color: kudos?.is_kudosed ? 'secondary.main' : 'neutral.white' }} />
-        </IconButton>
-      }
-      label={kudos && message(kudos)}
-      sx={{
-        bgcolor: 'neutral.whiteGrey',
-        color: 'neutral.white',
-        p: 0,
-      }}
-      clickable={false}
-      onClick={(e) => e.stopPropagation()}
-    />
+    <>
+      <Chip
+        disabled={!isKudosBtnShowing}
+        icon={
+          <IconButton aria-label={kudos !== null ? kudos.kudos_on_proof : ''} onClick={onClickHandler} sx={{ p: 0 }}>
+            <Star sx={{ fontSize: 28, color: kudos?.is_kudosed ? 'secondary.main' : 'neutral.white' }} />
+          </IconButton>
+        }
+        label={kudos && message(kudos)}
+        sx={{
+          bgcolor: 'neutral.whiteGrey',
+          color: 'neutral.white',
+          p: 0,
+        }}
+        clickable={false}
+        onClick={(e) => {
+          e.stopPropagation();
+          setOpenModal(true);
+        }}
+      />
+      <KudosGiveModal openModal={openModal} setOpenModal={setOpenModal} />
+    </>
   );
 };
